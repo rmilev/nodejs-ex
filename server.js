f58a2e4148f50e71495f38161a4b6a5f08a82839
Roberto Milev <robertomilev@robertosmacbook.lan>
2018-07-01 14:05:35 -0700

    Adding body parser

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
-    morgan  = require('morgan');
+    morgan  = require('morgan'),
+    bodyParser = require('body-parser');
 
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
-// app.use(express.bodyParser());
+app.use(bodyParser.json());
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
