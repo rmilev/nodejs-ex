e6c030c765cba07b28f3eb7293398dd3c0ef2f75
Roberto Milev <robertomilev@robertosmacbook.lan>
2018-07-01 01:00:32 -0700

    Test commit

--- server.js
+++ server.js
@@ -77,19 +77,8 @@ app.get('/', function (req, res) {
   }
 });
 
-app.get('/pagecount1', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
+app.get('/pagecount', function (req, res) {
+  res.send('{ pageCount: 1000}');
 });
 
 // error handling
